3c3d06c7cf2b133e446a19ca3a4a2732e25ffbc4
Kenean Dita <KeneanSufa@gmail.com>
2025-10-29 15:44:35 +0300

    review

--- src/app/publications/Resume/page.js
+++ src/app/publications/Resume/page.js
@@ -1,11 +1,10 @@
 import React from "react";
-import resumePDF from "../../../public/Resume.pdf";
 
 export default function PDFViewer() {
   return (
     <div className="w-full min-h-screen flex flex-col items-center justify-center bg-background p-4">
       <iframe
-        src={resumePDF}
+        src="/Resume.pdf"
         title="Resume"
         className="w-full max-w-4xl h-[80vh] border rounded shadow"
         style={{ minHeight: 600 }}
